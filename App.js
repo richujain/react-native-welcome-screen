89b2e9bbe91a4a72b603e75a53ee6ae011e70707
Richu <richujain1995@gmail.com>
2022-07-02 06:32:41 -0400

    Final commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import * as eva from '@eva-design/eva';
-import { StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, TouchableOpacity, ImageBackground, View } from 'react-native';
 import { 
   ApplicationProvider, 
   Layout, 
@@ -13,44 +13,38 @@ import {
 } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
+const image = { uri: 'https://i.imgur.com/VVYCHP4.jpg' }
+
+const HomeScreen = () =>  (
+  <Layout style={styles.container}>
+
+      <ImageBackground 
+        source={image}
+        style={styles.backgroundImage}
+      >
+    <View style={styles.waveContainer}>
+      <Image 
+        style={ styles.waves }
+        source={require("./assets/wave-haikei.png")}
+      />
+      <Button size="giant" status='warning' style={styles.button}>
+        Start Learning
+      </Button>
+      <Button size="giant" appearance='ghost' status='basic' style={styles.buttonSignIn}>
+        I ALREADY HAVE AN ACCOUNT
+      </Button>
+      <Text style={styles.text}>Start learning to code.</Text>
+    </View>
+      
+    </ImageBackground>
+  </Layout>
+);
 
 const StarIcon = (props) => <Icon {...props} name="star" />;
 
 export const BottomNavigationTabSimplecase = () => (
   <BottomNavigationTab title="USERS" icon={StarIcon} />
 );
-
-
-const HomeScreen = () => {
-  const [counter, setCounter] = React.useState(0);
-  return (
-  <Layout style={styles.container}>
-
-    <TouchableOpacity>
-      <Image 
-        source={{
-          width: 450,
-          height: 500,
-          uri: 'https://i.imgur.com/lZfagoB.jpg',
-        }}
-      />
-    </TouchableOpacity>
-    <Text style={styles.headerText} category={'h1'}>
-      React Native
-      </Text>
-
-      <Text style={styles.paragraphText}>
-      Pressed {counter} times
-      </Text>
-
-    <Button onPress={() => setCounter(counter + 1)}>Become a programmer!</Button>
-
-    <BottomNavigationTabSimplecase/>
-    
-  </Layout>
-)};
-
-
 
 export default () => (
   <>
@@ -65,14 +59,41 @@ export default () => (
 const styles = StyleSheet.create({
   container: {
     flex: 1, 
-    justifyContent: 'center', 
-     alignItems: 'center',
-     marginTop: 'auto',
+    flexDirection: "column"
   },
-  headerText: {
-    marginBottom: 16,
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    width: "100%",
+    height: "100%",
   },
-  paragraphText: {
-    marginBottom: 16,
-  }
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000a0",
+    zIndex: 1
+  },
+  button: {
+    //backgroundColor: "green",
+    borderRadius: 40,
+    position: "absolute",
+    bottom: 210,
+    borderColor: "transparent"
+  },
+  waves: {
+    // flex: 1,
+    // resizeMode: "stretch",
+    // justifyContent: "flex-start"
+  },
+  waveContainer:{
+    //flex: 1,
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+    bottom: -220,
+
+  },
+
 })
